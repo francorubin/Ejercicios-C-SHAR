d59c078dc2f06a3355031d6ae3fcbd7f74b55449
FranM17 <franmondi17@gmail.com>
2025-06-10 16:11:19 -0300

    Carpeta jugador cansado subida

---
+++ Ejs/Carpeta-Jugador-Cansado/Jugador Cansado/Jugador Cansado/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Jugador_Cansado
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
