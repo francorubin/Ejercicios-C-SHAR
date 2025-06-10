72dc6811b2a5377968df439cb6721b3881c1770a
Franco Rubin <rubinfranco07@gmail.com>
2025-06-10 16:01:39 -0300

    Carpeta con ejercicios C#

---
+++ Ejs/cronometro_RubinMondino/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+using System;
+
+using System;
+
+namespace cronometro_RubinMondino
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var cron = new Cronometro();
+            cron.Reiniciar();
+
+            bool salir = false;
+            while (!salir)
+            {
+                Console.WriteLine();
+                Console.WriteLine("1 Reiniciar cronómetro");
+                Console.WriteLine("2 Sumar 1 segundo");
+                Console.WriteLine("3 Mostrar tiempo");
+                Console.WriteLine("4 Simular 5000 segundos (desde cero)");
+                Console.WriteLine("5 Salir");
+                Console.Write("Opción: ");
+
+                var op = Console.ReadLine();
+                switch (op)
+                {
+                    case "1":
+                        cron.Reiniciar();
+                        Console.WriteLine("-> Reiniciado a 0.");
+                        break;
+
+                    case "2":
+                        cron.IncrementarTiempo();
+                        Console.WriteLine("-> +1 segundo.");
+                        break;
+
+                    case "3":
+                        Console.WriteLine("Tiempo actual: " + cron.MostrarTiempo());
+                        break;
+
+                    case "4":
+                        cron.Reiniciar();              
+                        for (int i = 0; i < 5000; i++)
+                        {
+                            cron.IncrementarTiempo();
+                        }
+                        Console.WriteLine("Después de 5000 s: " + cron.MostrarTiempo());
+                        break;
+
+                    case "5":
+                        salir = true;
+                        break;
+
+                    default:
+                        Console.WriteLine("Opción inválida.");
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ Ejs/cronometro_RubinMondino/cronometro.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace cronometro_RubinMondino
+{
+    public class Cronometro
+    {
+        private int segundos;
+        private int minutos;
+
+        public void Reiniciar()
+        {
+            segundos = 0;
+            minutos = 0;
+        }
+
+        public void IncrementarTiempo()
+        {
+            segundos++;
+            if (segundos >= 60)
+            {
+                segundos -= 60;
+                minutos++;
+            }
+        }
+
+        public string MostrarTiempo()
+        {
+            return $"{minutos} minutos, {segundos} segundos";
+        }
+    }
+}
